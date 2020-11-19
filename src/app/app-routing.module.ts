001f95bb33e200a3fa4a764722de5f8f846f82fa
Connor Robbins <connor.robbins2121@gmail.com>
2020-11-19 04:41:22 +0000

    Maria lost 5 on this commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { CreatePostComponent } from './create-post/create-post.component';
 import { CategoryPageComponent } from './category-page/category-page.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { ChatBotComponent} from './chat-bot/chat-bot.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
 
 const routes: Routes = [
@@ -13,7 +14,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'sign-up', component: CreateAccountComponent},
   { path: 'forum/:category', component: CategoryPageComponent },
-  { path: 'menu', component: MenuComponent}
+  { path: 'menu', component: MenuComponent},
+  { path: 'chat-bot', component: ChatBotComponent}
 ];
 
 @NgModule({
--- src/app/chat-bot/chat-bot.component.ts
+++ src/app/chat-bot/chat-bot.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare const window: any; //Maria lost $25 on this statement
 
 @Component({
   selector: 'app-chat-bot',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatBotComponent implements OnInit {
 
-  constructor() { }
+  constructor() { } 
 
   ngOnInit() {
+
+    window.watsonAssistantChatOptions = {
+        integrationID: "4e54cd19-756d-436f-a94b-c63d54ff98d3", // The ID of this integration.
+        region: "us-south", // The region your integration is hosted in.
+        serviceInstanceID: "b41622e1-8511-4076-be9c-0f76647053e7", // The ID of your service instance.
+        onLoad: function(instance) { instance.render(); }
+      };
+    setTimeout(function(){
+      const t=document.createElement('script');
+      t.src="https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js";
+      document.head.appendChild(t);
+    });
+
+
   }
 
 }
+
+
